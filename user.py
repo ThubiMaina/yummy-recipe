6e7e78f7f64aa69de66cbeac0461c630ba763808
Erick <erickmaina35@gmail.com>
2017-10-17 12:21:38 +0300

    [UPDATE] linting my code to follow PEP8

--- user.py
+++ user.py
@@ -1,12 +1,14 @@
+""" The user class functions"""
 import re
-
+# pylint: disable=R0201
+# pylint: disable=invalid-name
 users = {'me@gmail.com': {'password': 'me@gmail.com', 'username': 'Mercy', 'email': 'me@gmail.com'}}
 
 class User(object):
     """
     Class to handle  user functions
     """
-
+    # pylint: disable-msg=C0103
     def __init__(self, username=None, email=None, password=None, cpassword=None):
         """ Initializing  class instance variables"""
         self.username = username
@@ -15,27 +17,26 @@ class User(object):
         self.cpassword = cpassword
 
     def register(self, email, username, password, cpassword):
-        """defining method to create account"""           
+        """defining method to create account"""
         if username != '' and email != '' and password != '':
             if re.match("[a-zA-Z0-9- .]+$", username):
                 if email not in users.keys():
                     if password == cpassword:
                         regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
                         result = email
-                        if re.search(regex ,result):
+                        if re.search(regex, result):
                             users[email] = {'email':email,
                                             'username':username,
-                                            'password':password
-                                            }
+                                            'password':password}
                             print(users)
-                            return 1 
-                        return 2    
+                            return 1
+                        return 2
                     return 3
                 return 4
             return 5
         return 6
-             
-    def login(self,email, password_entered):
+
+    def login(self, email, password_entered):
         """ defining method to Log in user"""
         if email != '' and password_entered != '':
             if email in users.keys():
@@ -46,19 +47,17 @@ class User(object):
                 return 2
             return 3
         return 4
-            
+
     def get_user_name(self, email):
         """function to get a user's name"""
         if email in users.keys():
             result = users[email]
             return result['username']
-        else:
-            return False
+        return False
 
     def get_user_email(self, email):
         """function to get a user's email"""
         if email in users.keys():
             result = users[email]
             return result['email']
-        else:
-            return False
+        return False
