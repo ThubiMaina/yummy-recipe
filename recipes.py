3501ffa35cddff364246432ef19941ba8eec08bf
Erick <erickmaina35@gmail.com>
2017-10-18 11:18:01 +0300

    added the create category function

--- recipes.py
+++ recipes.py
@@ -10,4 +10,28 @@ class RecipeCat(object):
         self.category = category
         self.owner = owner
 
+    def create(self, category, owner):
+        """defining method to create recipe category list"""
+        if category != '':
+            #call the get_myrecipe_lists function that contains individual recipe categories 
+            my_recipecats = self.get_myrecipe_lists(owner)
+            if my_recipecats != {}:
+                #check's if user already has a category added
+                if category not in my_recipecats.keys():
+                    self.Recipecats[category] = {
+                    'category':category,
+                    'owner':owner,
+                    }
+                    return 1
+                else:
+                    return 2
+            else:
+                self.Recipecats[category] = {
+                'category':category,
+                    'owner':owner,
+                }
+                return 1
+        else:
+            return 3
+
     
