0a63995eea7ece45a1eba3186032b8b2435b1f2b
Erick <erickmaina35@gmail.com>
2017-10-19 14:06:11 +0300

    [UPDATE] code to follow pep 8

--- user.py
+++ user.py
@@ -9,17 +9,16 @@ class User(object):
     Class to handle  user functions
     """
     # pylint: disable-msg=C0103
-    def __init__(self, username = None, email = None, password= None, cpassword=None):
+    def __init__(self, username=None, email=None, password=None, cpassword=None):
         """ Initializing  class instance variables"""
         self.username = username
         self.email = email
         self.password = password
         self.cpassword = cpassword
-        
     def register(self, email, username, password, cpassword):
         """defining method to create account"""
-        if username != '' and email != '' and password != '':    
-            if re.match("[a-zA-Z0-9- .]+$",username):
+        if username != '' and email != '' and password != '':
+            if re.match("[a-zA-Z0-9- .]+$", username):
                 if email not in users.keys():
                     if username not in users.keys():
                         if password == cpassword:
@@ -27,17 +26,13 @@ class User(object):
                             result = email
                             if re.search(regex, result):
                                 if len(password) < 6:
-                                
-                                
                                     users[email] = {
                                         'username': username,
                                         'email': email,
-                                        'password': password,
-                                    }
-                        
+                                        'password': password,}
                                     print(users.keys)
                                     return 1
-                                return "password less than 6"       
+                                return "password less than 6"
                             return "invalid email"
                         return "password mismatch"
                     return "username exists"
@@ -53,9 +48,9 @@ class User(object):
                 pword = result['password']
                 if pword == password:
                     return 1
-                return 2 
-            return 3 
-        return 4 
+                return 2
+            return 3
+        return 4
 
     def get_user_name(self, email):
         """function to get a user's name"""
